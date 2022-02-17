21f030bba38fb7e472efc66ec9e78be5662c562a
JonasJore <jonas.jore@gmail.com>
2022-02-17 10:05:20 +0100

    #3 added a view for adding new todos

--- app/src/main/java/com/jonasjore/simple_todo_app/AddTodoScreen.kt
+++ app/src/main/java/com/jonasjore/simple_todo_app/AddTodoScreen.kt
@@ -1,19 +1,49 @@
 package com.jonasjore.simple_todo_app
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jonasjore.simple_todo_app.ui.theme.SimpleTodoAppTheme
 
 @Composable
 fun AddTodoScreen(onBack: () -> Unit) {
+    var todoText by remember { mutableStateOf("") }
+    val context = LocalContext.current
     Scaffold(topBar = { TodoAppTopBar(onBack = onBack) }, content = {
         Surface {
             Column {
                 H3(text = "New TODO")
+                TextField(
+                    value = todoText,
+                    onValueChange = { todoText = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    label = { Text("New TODO") }
+                )
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = {
+                            Toast.makeText(context, "something", Toast.LENGTH_SHORT).show()
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = Color.Cyan,
+                            contentColor = Color.Black
+                        ),
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("SUBMIT TODO")
+                    }
+                }
             }
         }
     })
